8b405cde40f06f95e4ff2679857c1f3c911887ff
John Wormald <johnwormald@outlook.com>
2024-01-03 15:41:10 +0000

    Empty Migrations

--- Macs.WebApi.DataAccess/MacsContext.cs
+++ Macs.WebApi.DataAccess/MacsContext.cs
@@ -20,49 +20,149 @@ namespace Macs.WebApi.DataAccess.Entities
         {
             base.OnModelCreating(modelBuilder);
 
-            var guid1 = Guid.NewGuid();
+            var personId1 = Guid.NewGuid();
+            var personId2 = Guid.NewGuid();
+            var personId3 = Guid.NewGuid();
+            var personId4 = Guid.NewGuid();
 
             modelBuilder.Entity<Address>()
                 .HasData(
-                        new Address()
+                        new ()
                         {
                             Id = Guid.NewGuid(),
-                            PersonId = guid1,
-                            AddressLine1 = "Address Line 1",
-                            AddressLine2 = "Address Line 2",
-                            AddressLine3 = "Address Line 3",
-                            PostCode = "1ED",
+                            PersonId = personId1,
+                            AddressLine1 = "Heriot-Watt University",
+                            AddressLine2 = "",
+                            AddressLine3 = "",
+                            PostCode = " EH14 4AS",
                             City = "Edinburgh",
                             Country = "Scotland",
                             IsActive = true,
                             Preferred = true,
-                            Type = AddressType.Home
+                            Type = AddressType.Work
+                        },
+                        new ()
+                        {
+                            Id = Guid.NewGuid(),
+                            PersonId = personId2,
+                            AddressLine1 = "Heriot-Watt University",
+                            AddressLine2 = "",
+                            AddressLine3 = "",
+                            PostCode = " EH14 4AS",
+                            City = "Edinburgh",
+                            Country = "Scotland",
+                            IsActive = true,
+                            Preferred = true,
+                            Type = AddressType.Work
+                        },
+                        new ()
+                        {
+                            Id = Guid.NewGuid(),
+                            PersonId = personId3,
+                            AddressLine1 = "Heriot-Watt University",
+                            AddressLine2 = "",
+                            AddressLine3 = "",
+                            PostCode = " EH14 4AS",
+                            City = "Edinburgh",
+                            Country = "Scotland",
+                            IsActive = true,
+                            Preferred = true,
+                            Type = AddressType.Work
+                        },
+                        new ()
+                        {
+                            Id = Guid.NewGuid(),
+                            PersonId = personId4,
+                            AddressLine1 = "Heriot-Watt University",
+                            AddressLine2 = "",
+                            AddressLine3 = "",
+                            PostCode = " EH14 4AS",
+                            City = "Edinburgh",
+                            Country = "Scotland",
+                            IsActive = true,
+                            Preferred = true,
+                            Type = AddressType.Work
                         }
                 );
 
             modelBuilder.Entity<Contact>()
                 .HasData(
-                new Contact()
+                new ()
                 {
                     Id = Guid.NewGuid(),
-                    PersonId = guid1,
+                    PersonId = personId1,
                     IsActive = true,
                     Preferred = true,
                     Type = ContactType.Email,
-                    Value = "mirela@home.com"
+                    Value = "mirela@hw.com"
+                },
+                new ()
+                {
+                    Id = Guid.NewGuid(),
+                    PersonId = personId2,
+                    IsActive = true,
+                    Preferred = true,
+                    Type = ContactType.Email,
+                    Value = "nastia@hw.com"
+                },
+                new ()
+                {
+                    Id = Guid.NewGuid(),
+                    PersonId = personId3,
+                    IsActive = true,
+                    Preferred = true,
+                    Type = ContactType.Email,
+                    Value = "paul@hw.com"
+                },
+                new ()
+                {
+                    Id = Guid.NewGuid(),
+                    PersonId = personId4,
+                    IsActive = true,
+                    Preferred = true,
+                    Type = ContactType.Email,
+                    Value = "john@hw.com"
                 }
                 );
 
             modelBuilder.Entity<Person>()
-                .HasData(new Person()
-                {
-                    Id = guid1,
-                    Title = "Ms",
-                    FirstName = "Mirela",
-                    MiddleName = "Lucia",
-                    LastName = "Sauca",
-                    DateOfBirth = new DateTime(2000, 1,1)
-                });
+                .HasData(
+                    new ()
+                    {
+                        Id = personId1,
+                        Title = "Ms",
+                        FirstName = "Mirela",
+                        MiddleName = "",
+                        LastName = "Developer",
+                        DateOfBirth = new DateTime(1990, 1,1)
+                    },
+                    new ()
+                    {
+                        Id = personId2,
+                        Title = "Miss",
+                        FirstName = "Nastia",
+                        MiddleName = "",
+                        LastName = "Developer",
+                        DateOfBirth = new DateTime(2000, 1, 1)
+                    },
+                    new ()
+                    {
+                        Id = personId3,
+                        Title = "Mr",
+                        FirstName = "Paul",
+                        MiddleName = "",
+                        LastName = "Developer",
+                        DateOfBirth = new DateTime(1990, 1, 1)
+                    },
+                    new ()
+                    {
+                        Id = personId4,
+                        Title = "Mr",
+                        FirstName = "John",
+                        MiddleName = "",
+                        LastName = "Developer",
+                        DateOfBirth = new DateTime(1980, 1, 1)
+                    });
         }
     }
 
